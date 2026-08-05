f654a71dc12932fe78cf48cfddd5952c0fc0c49f
Soner Türkoğlu <sonerturkoglu10@gmail.com>
2026-08-05 13:09:53 +0300

    Add STATIC_ROOT

--- aykart/settings.py
+++ aykart/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')] 
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 # Media files (Uploads)
